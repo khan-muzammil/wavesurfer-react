f1cbac7669a4ed9c453587dd14751deb4664debf
Muzammil Khan <muzammil.khan@outlook.in>
2021-04-06 10:52:40 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import {
+	AppBar,
+	Box,
+	Container,
+	CssBaseline,
+	makeStyles,
+	Toolbar,
+	Typography,
+} from "@material-ui/core"
+
+const useStyles = makeStyles({
+	title: {
+		flexGrow: 1,
+	},
+})
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const classes = useStyles()
+	return (
+		<>
+			<AppBar position="fixed">
+				<Toolbar variant="dense">
+					<Typography variant="h6" className={classes.title}>
+						Deepsync-Wavesurfer
+					</Typography>
+				</Toolbar>
+			</AppBar>
+
+			{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+			<CssBaseline />
+			<Container>
+				<Box marginTop={10}>
+					<h1>Hello World</h1>
+				</Box>
+			</Container>
+		</>
+	)
 }
 
-export default App;
+export default App
